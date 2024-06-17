88b51f2530950a634bb2cb5b13a73a53af6935fb
Erik Dubois <plackovicdajan@gmail.com>
2024-06-17 10:45:25 +0200

    even errors out if not solvable, im a cretin

--- main.c
+++ main.c
@@ -6,12 +6,16 @@ int solve(char grid[6][6]);
 int main(int argc, char *argv[])
 {
 	char input[6][6];
+	int solvable;
 
 	if (argc == 2)
 	{
 		parse(input, argv[1]);
-		solve(input);
-		print_grid(input);
+		solvable = solve(input);
+		if (solvable)
+			print_grid(input);
+		else
+			error_out();
 	}
 	else
 		error_out();
